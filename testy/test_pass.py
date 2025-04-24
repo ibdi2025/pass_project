51917d3f0b158d1fb6a8e39afe62af3a97d191f9
ibdi2025 <dibran.ibisi@gmail.com>
2025-04-24 20:04:40 +0200

    Version of _c_Users_DELL_Documents_RD_Python_pass_project, Thu Apr 24 20:04:40 CEST 2025

--- testy/test_pass.py
+++ testy/test_pass.py
@@ -1,8 +1,20 @@
 from Projekt_DI import Sha
+from Projekt_DI import Lister
 
-
-def test_pass():
+def test_hash_it():
     a = Sha('a', 'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb')
     result=a.hash_it()
     expected='ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb'
     assert expected==result
+
+
+def test_read():
+    b= Lister()
+    try:
+        b.read()
+        result = 1
+    except FileExistsError:
+        result=0
+
+    expected=1
+    assert expected == result
