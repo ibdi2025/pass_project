fc0b695265513665dc3da9668fb0dfbf4f5aa735
ibdi2025 <dibran.ibisi@gmail.com>
2025-04-25 08:22:28 +0200

    Version of _c_Users_DELL_Documents_RD_Python_pass_project, Fri Apr 25 08:22:27 CEST 2025

--- testy/test_pass.py
+++ testy/test_pass.py
@@ -2,7 +2,7 @@ from Projekt_DI import Sha
 from Projekt_DI import Lister
 
 def test_hash_it():
-    a = Sha('a', 'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb')
+    a = Sha('a', None)
     result=a.hash_it()
     expected='ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb'
     assert expected==result
